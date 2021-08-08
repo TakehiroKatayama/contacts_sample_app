d5893d1f441fdf93faf2edc6dd4b1eac5120d562
TakehiroKatayama <t.katayama.info@gmail.com>
2021-08-09 00:12:00 +0900

    管理者ページにてContactを確認編集できるように設定

---
+++ app/admin/contacts.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Contact do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :email, :phonenumber, :message, :remote_ip
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :email, :phonenumber, :message, :remote_ip]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  permit_params :name, :email, :phonenumber, :message, :remote_ip
+end
