037732e738ad4eac8be808b5e4349ec09af88513
TakehiroKatayama <t.katayama.info@gmail.com>
2021-08-10 03:04:58 +0900

    新規投稿時にslack通知機能を追加

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,15 +5,42 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(contact_params)
-    if @contact.save
-      # ユーザーにメールを送信
-      ContactMailer.user_email(@contact).deliver_now
-      # 管理者にメールを送信
-      ContactMailer.admin_email(@contact).deliver_now
-      redirect_to root_path
-    else
-      render :index
+
+    respond_to do |format|
+      if @contact.save
+        # ユーザーにメールを送信
+        ContactMailer.user_email(@contact).deliver_now
+        # 管理者にメールを送信
+        ContactMailer.admin_email(@contact).deliver_now
+        notify_to_slack(@contact)
+        format.html { redirect_to root_path, notice: 'Contact was successfully created.' }
+        format.json { render :index, status: :created, location: @contact }
+        # redirect_to root_path
+      else
+        format.html { render :index, status: :unprocessable_entity }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+        # render :index
+      end
     end
+  end
+
+  def notify_to_slack(contact)
+    message = <<~EOS
+      新しいお問い合わせがありました！
+      ```
+      name: #{contact.name}
+      email: #{contact.email}
+      phonenumber: #{contact.phonenumber}
+      message: #{contact.message}
+      ```
+    EOS
+
+    notifier = Slack::Notifier.new(
+      ENV['SLACK_WEBHOOK_URL'],
+      channel: "##{ENV['SLACK_CHANNEL']}",
+      username: 'お問い合わせ通知'
+    )
+    notifier.ping message
   end
 
   private
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ApplicationMailer
-  ADMIN_EMAIL = 'rails.sample.mail.test@gmail.com'
+  ADMIN_EMAIL = 'admin@example.com'
 
   def user_email(contact)
     @contact = contact
